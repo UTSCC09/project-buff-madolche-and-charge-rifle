5fc42024a02affd169428275b1a425d449910d8d
Zhaohang Yan <56213581+ymart1n@users.noreply.github.com>
2022-03-08 01:10:00 -0500

    implementing basic peerjs

--- md-together/src/components/EditorComponent.js
+++ md-together/src/components/EditorComponent.js
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import MDEditor from '@uiw/react-md-editor';
+// Use Peer.js to realize Web real time communication
+import Peer from 'peerjs';
 
 // Used ImgBB https://imgbb.com/ to host our logo for free
 let defaultWelcome = `# Welcome to mdTogether!
@@ -14,6 +16,11 @@ let defaultWelcome = `# Welcome to mdTogether!
 - Change the **Theme** that best fit your mood today!
 `
 
+// Peer.js functionality is referred to Getting started with PeerJS example
+// https://blog.logrocket.com/getting-started-peerjs/
+const peer = new Peer();
+let conn;
+
 // using react-md-editor as the markdown editor
 // this component is built on the top of example given in react-md-editor documentation
 // https://uiwjs.github.io/react-md-editor/
@@ -21,11 +28,71 @@ export default function EditorComponent() {
   // The Current State of Markdown File
   const [value, setValue] = React.useState(defaultWelcome);
   // console.log(value);
+  // console.log(peer);
+  
+  // This form is referred to W3Schools React Form example as a template
+  // https://www.w3schools.com/react/react_forms.asp
+  const [connectId, setConnectId] = useState("");
+  const [currId, setCurrId] = useState("");
+  const [currConn, setCurrConn] = useState(conn);
+
+  // console.log(currConn);
+
+  peer.on('open', (id) => {
+    setCurrId(id);
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const conn = peer.connect(connectId);
+    conn['caller'] = peer.id;
+    setCurrConn(conn);
+    console.log("im caller");
+    conn.on('data', (data) => {
+      setValue(data);
+    });
+    setConnectId("");
+  }
+
+  
+  React.useEffect(() => {
+    conn = currConn;
+
+    peer.on('connection', (conn) => {
+      console.log("im callee");
+      conn['caller'] = conn.peer;
+      setCurrConn(conn);
+      conn.on('data', (data) => {
+        setValue(data);
+      });
+    });
+  });
+
+
+  function handleEditorChange(value) {
+    if (currConn) {
+      currConn.send(value);
+      currConn.on('data', (data) => {
+        setValue(data);
+      });
+    }
+    setValue(value);
+  }
+
+
   return (
     <div>
+      {/* <PeerConnectionForm /> */}
+      <form onSubmit={handleSubmit}>
+        <label>Your Current Peer ID is: {currId}</label>
+        <label>Enter the peer id to connect:
+          <input id="connectId" type="text" value={connectId} onChange={(e) => setConnectId(e.target.value)}></input>
+        </label>
+        <input type="submit"></input>
+      </form>
       <MDEditor
         value={value}
-        onChange={setValue}
+        onChange={handleEditorChange}
         visiableDragbar={false}
         height={"90vh"}
       />
