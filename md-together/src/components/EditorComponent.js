e1d1e77f4ceb96ee8d38c26895a055ba8ceaafad
Zhaohang Yan <56213581+ymart1n@users.noreply.github.com>
2022-03-13 01:13:06 -0500

    implement basic video calling feature

--- md-together/src/components/EditorComponent.js
+++ md-together/src/components/EditorComponent.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import MDEditor from '@uiw/react-md-editor';
 // Use Peer.js to realize Web real time communication
 import Peer from 'peerjs';
+import ReactSession from 'react-client-session/dist/ReactSession';
 
 // Used ImgBB https://imgbb.com/ to host our logo for free
 let defaultWelcome = `# Welcome to mdTogether!
@@ -16,15 +17,24 @@ let defaultWelcome = `# Welcome to mdTogether!
 - Change the **Theme** that best fit your mood today!
 `
 
+// let currEmail = ReactSession.get("email");
+
 // Peer.js functionality is referred to Getting started with PeerJS example
 // https://blog.logrocket.com/getting-started-peerjs/
 const peer = new Peer();
-let conn;
+let dataConn;
+
+// codes derived from peer.js video/audio call example
+// https://blog.logrocket.com/getting-started-peerjs/
+let mediaConn;
 
 // using react-md-editor as the markdown editor
 // this component is built on the top of example given in react-md-editor documentation
 // https://uiwjs.github.io/react-md-editor/
 export default function EditorComponent() {
+
+  let peerVideo;
+
   // The Current State of Markdown File
   const [value, setValue] = React.useState(defaultWelcome);
   // console.log(value);
@@ -32,9 +42,11 @@ export default function EditorComponent() {
   
   // This form is referred to W3Schools React Form example as a template
   // https://www.w3schools.com/react/react_forms.asp
-  const [connectId, setConnectId] = useState("");
+  const [dataConnId, setDataConnId] = useState("");
+  const [mediaConnId, setMediaConnId] = useState("");
   const [currId, setCurrId] = useState("");
-  const [currConn, setCurrConn] = useState(conn);
+  const [currDataConn, setCurrDataConn] = useState(dataConn);
+  // const [currMediaConn, setCurrMediaConn] = useState(mediaConn);
 
   // console.log(currConn);
 
@@ -42,54 +54,130 @@ export default function EditorComponent() {
     setCurrId(id);
   });
 
-  const handleSubmit = (e) => {
+  const handleDataSubmit = (e) => {
     e.preventDefault();
-    const conn = peer.connect(connectId);
-    conn['caller'] = peer.id;
-    setCurrConn(conn);
+    const dataConn = peer.connect(dataConnId);
+    setCurrDataConn(dataConn);
     console.log("im caller");
-    conn.on('data', (data) => {
+    dataConn.on('data', (data) => {
       setValue(data);
     });
-    setConnectId("");
+    setDataConnId("");
   }
 
-  
   React.useEffect(() => {
-    conn = currConn;
+    dataConn = currDataConn;
 
-    peer.on('connection', (conn) => {
+    peer.on('connection', (dataConn) => {
       console.log("im callee");
-      conn['caller'] = conn.peer;
-      setCurrConn(conn);
-      conn.on('data', (data) => {
+      setCurrDataConn(dataConn);
+      dataConn.on('data', (data) => {
         setValue(data);
       });
     });
   });
 
+  // codes derived from peer.js video/audio call example
+  // https://blog.logrocket.com/getting-started-peerjs/
+  function showVideo(stream) {
+    peerVideo.srcObject = stream;
+  }
+
+  const handleMediaSubmit = (e) => {
+    e.preventDefault();
+    let mediaConn = peer.connect(mediaConnId);
+    // setCurrMediaConn(mediaConn);
+    // console.log(mediaConn);
+    navigator.mediaDevices.getUserMedia({video: true, audio: true})
+      .then((stream) => {
+        let call = peer.call(mediaConnId, stream);
+        call.on('stream', showVideo);
+      })
+      .catch((err) => {
+        console.log('Failed to get local stream', err);
+      });
+    console.log("im caller");
+    setMediaConnId("");
+  }
+  
+  React.useEffect(() => {
+    peerVideo = document.getElementById("peer_video");
+    peer.on('call', (call) => {
+      navigator.mediaDevices.getUserMedia({video: true, audio: true})
+        .then((stream) => {
+          call.answer(stream); // Answer the call with an A/V stream.
+          call.on('stream', showVideo);
+        })
+        .catch((err) => {
+          console.error('Failed to get local stream', err);
+        });
+    });
+  });
 
   function handleEditorChange(value) {
-    if (currConn) {
-      currConn.send(value);
-      currConn.on('data', (data) => {
+    if (currDataConn) {
+      currDataConn.send(value);
+      currDataConn.on('data', (data) => {
         setValue(data);
       });
     }
     setValue(value);
   }
 
+  let isLoggedIn = false;
+
+  // example derived from w3schools example
+  // https://www.w3schools.blog/get-cookie-by-name-javascript-js
+  let cookie = {};
+  document.cookie.split(";").forEach(function(elmt) {
+    let [key, value] = elmt.split("=");
+    cookie[key.trim()] = value;
+  });
+
+  if (cookie["__react_session__"] && ReactSession.get("userId") !== null && ReactSession.get("userId") !== undefined) {
+    isLoggedIn = true;
+  } else {
+    isLoggedIn = false;
+  }
+
+  function DataPeerForm() {
+    return (
+      <div>
+        <label>Your Current Peer ID is: {currId}</label>
+        <form onSubmit={handleDataSubmit}>
+          <label>Enter the peer id to collaborate on writing:
+            &nbsp;
+            <input id="dataConnId" type="text" value={dataConnId} onChange={(e) => setDataConnId(e.target.value)}></input>
+            &nbsp;
+          </label>
+          <input type="submit"></input>
+        </form>
+        <form onSubmit={handleMediaSubmit}>
+          <label>Enter the peer id to call:
+              &nbsp;
+              <input id="mediaConnId" type="text" value={mediaConnId} onChange={(e) => setMediaConnId(e.target.value)}></input>
+              &nbsp;
+            </label>
+          <input type="submit"></input>
+        </form>
+      </div>
+    );
+  }
+
+  function PeerSection(props) {
+    const isLoggedIn = props.isLoggedIn;
+    if (isLoggedIn) {
+      return <DataPeerForm />;
+    } else {
+      return (<div>Please sign in first.</div>);
+    }
+  }
 
   return (
     <div>
-      {/* <PeerConnectionForm /> */}
-      <form onSubmit={handleSubmit}>
-        <label>Your Current Peer ID is: {currId}</label>
-        <label>Enter the peer id to connect:
-          <input id="connectId" type="text" value={connectId} onChange={(e) => setConnectId(e.target.value)}></input>
-        </label>
-        <input type="submit"></input>
-      </form>
+      <PeerSection isLoggedIn={isLoggedIn} />
+      <video id="peer_video" width={200} height={200} autoPlay />
+
       <MDEditor
         value={value}
         onChange={handleEditorChange}
