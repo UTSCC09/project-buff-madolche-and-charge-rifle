0cc44e62facf1febd4c87ad44a26e158b42688e3
Zhaohang Yan <56213581+ymart1n@users.noreply.github.com>
2022-03-29 11:23:28 -0400

    getting rid of webfonts

--- md-together/src/App.js
+++ md-together/src/App.js
@@ -11,7 +11,6 @@ import ReactSession from 'react-client-session/dist/ReactSession';
 import React, { useState, useEffect } from 'react';
 import styled, { ThemeProvider } from "styled-components";
 import { GlobalStyles } from './theme/GlobalStyles';
-import WebFont from 'webfontloader';
 import { useTheme } from './theme/useTheme';
 
 const Container = styled.div`
@@ -22,21 +21,13 @@ const Container = styled.div`
 function App() {
   ReactSession.setStoreType('cookie');
 
-  const {theme, themeLoaded, getFonts} = useTheme();
+  const {theme, themeLoaded } = useTheme();
   const [selectedTheme, setSelectedTheme] = useState(theme);
 
   useEffect(() => {
     setSelectedTheme(theme);
-   }, [themeLoaded]);
-
-  // 4: Load all the fonts
-  useEffect(() => {
-    WebFont.load({
-      google: {
-        families: getFonts()
-      }
-    });
-  });
+   }, [themeLoaded, theme]
+  );
 
   return (
     //<BrowserRouter>
